34295722dd96fd12ef9de86f120440f8b452e40d
EC2 Default User <ec2-user@ip-172-31-42-179.us-west-2.compute.internal>
2020-09-15 14:01:21 +0000

    new login page, chartkick fix in home page

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,6 @@ module GymApp
     config.i18n.available_locales = ["pt-BR"]
     config.i18n.default_locale = "pt-BR"
     Groupdate.time_zone = false
-    Faker::Config.locale = 'en'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
